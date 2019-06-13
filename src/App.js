12f19e9e21b518a49db0dc736ced06ff6d22d1fd
Carlos Segoviano <halokid626@hotmail.com>
2019-06-12 18:23:42 -0700

    added more files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,86 @@
 import React from 'react';
-import logo from './logo.svg';
+import Split from './composition/Split';
 import './App.css';
+import Tooltip from './composition/Tooltip';
+import Messages from './Messages';
+import TheDate from './state/TheDate';
+import Counter from './state/Counter';
+import HelloWorld from './state-drills/HelloWorld';
+
+
+const firstTooltip = (
+  <Tooltip color='hotpink' message='tooltip message'>
+   ipsum
+  </Tooltip>
+)
+const secondTooltip = (
+  <Tooltip color='#126BCC' message='another tooltip message'>
+    officiis
+  </Tooltip>
+)
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  class TheDate extends React.Component {
+    constructor(props) {
+      super(props)
+      this.state = { datetime: new Date() };
+      console.log('constructor')
+      }
+      componentDidMount() {
+        console.log('componentDidMount')
+        this.interval = setInterval(() => {
+          console.log('setInterval')
+          this.setState({
+            datetime:  new Date()
+          })
+        }, 1000)
+      }
+      componentWillUnmount() {
+        clearInterval(this.interval)
+      }
 
-export default App;
+    render() {
+      console.log('render')
+      return (
+        <div>{this.state.datetime.toLocaleString()}</div>
+      )
+    }
+  }
+
+  class Counter extends React.Component {
+    constructor(props) {
+      super(props)
+      this.state = {
+        count: 0
+      }
+    }
+
+    handleButtonClick = () => {
+      const newCount = this.state.count + 1
+      this.setState({
+        count: newCount
+      })
+    }
+    render() {
+      return (
+        <div>
+          <p>The current count: {this.state.count}</p>
+          <button onClick={this.handleButtonClick}>
+          Add 1
+          </button>
+        </div>
+      )
+    }
+  }
+
+  return (
+    <div>
+    <TheDate></TheDate>
+    <Counter count={123} />
+    <HelloWorld></HelloWorld>
+    </div>
+  )
+  }
+
+  export default App;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,128 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+export default class HelloWorld extends React.Component {
+  state = {
+    who: 'world',
+  };
+
+  render() {
+    return (
+      <div className='HelloWorld'>
+        <p>Hello, {this.state.who}!</p>
+        <button
+          onClick={() => this.setState({ who: 'world' })}
+        >
+          World
+        </button>
+        <br/>
+        <button
+          onClick={() => this.setState({ who: 'friend' })}
+        >
+          Friend
+        </button>
+        <br />
+        <button
+          onClick={() => this.setState({ who: 'React' })}
+        >
+          React
+        </button>
+      </div>
+    )
+  }
+}
+
+class Bomb extends React.Component {
+  state = {
+    count: 0,
+  }
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState({
+        count: this.state.count + 1
+      })
+    }, 1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval)
+  }
+
+  renderDisplay() {
+    const { count } = this.state
+    if (count >= 8) {
+      clearInterval(this.interval)
+      return 'BOOM!!!!'
+    } else if (count % 2 === 0) {
+      return 'tick'
+    } else {
+      return 'tock'
+    }
+  }
+
+  render() {
+    return (
+      <div className='CountdownBomb'>
+        {this.renderDisplay()}
+      </div>
+    )
+  }
+
+}
+
+class RouletteGun extends React.Component {
+  static defaultProps = {
+    bulletInChamber: 8
+  };
+
+  state = {
+    chamber: null,
+    spinningTheChamber: false,
+  };
+
+  componentWillUnmount() {
+    clearTimeout(this.timeout)
+  }
+
+  handleClick = () => {
+    this.setState({
+      spinningTheChamber: true,
+    })
+    this.timeout = setTimeout(() => {
+      const randomChamber = Math.ceil(Math.random() * 8)
+
+      this.setState({
+        chamber: randomChamber,
+        spinningTheChamber: false,
+      })
+    }, 2000)
+  }
+
+  renderDisplay() {
+    const { chamber, spinningTheChamber } = this.state
+    const { bulletInChamber } = this.props
+    if (spinningTheChamber) {
+      return 'spinning the chamber and pulling the trigger! ...'
+    } else if (chamber === bulletInChamber) {
+      return 'BANG!!!!!'
+    } else {
+      return 'you\'re safe!'
+    }
+  }
+
+  render() {
+    return (
+      <div className='RouletteGun'>
+        <p>{this.renderDisplay()}</p>
+        <button onClick={this.handleClick}>
+          Pull the trigger!
+        </button>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<RouletteGun />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
